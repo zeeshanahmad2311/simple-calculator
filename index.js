83fa707402dfe4424262ace464b884b4c07a396d
zeeshan ahmad <152972620+zeeshanahmad2311@users.noreply.github.com>
2024-05-31 12:36:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", name: "firstNumber" },
+    { message: "enter secand number", name: "secandNumber" },
+    { message: "select one of the operator", type: "list", name: "operator", choices: ["addition", "multiplication", "subtraction", "division"] }
+]);
+if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secandNumber);
+}
+else if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secandNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secandNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secandNumber);
+}
+else {
+    console.log("please enter your number");
+}
